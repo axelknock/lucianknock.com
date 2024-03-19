ae8b7d29298305cb6c1b964af0f577024b258a9f
Lucian Knock <me@lucianknock.com>
2024-03-19 19:50:04 -0400

    Add local denote variables

---
+++ content/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables.  For more information evaluate:
+;;;
+;;;     (info "(emacs) Directory Variables")
+
+((nil . ((denote-directory . "/home/lucian/Nextcloud/H/hugo/lucianknock.com/content/")
+         (denote-prompts . (subdirectory title keywords))
+         (denote-file-type . markdown-toml))))
