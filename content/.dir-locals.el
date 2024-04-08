2146486178828563ba12c5118fa92e1379a84cfb
Lucian Knock <github@lucianknock.com>
2024-04-08 19:05:19 -0400

    Add useful starting point to content

--- content/.dir-locals.el
+++ content/.dir-locals.el
@@ -4,4 +4,7 @@
 
 ((nil . ((denote-directory . "/home/lucian/Nextcloud/H/hugo/lucianknock.com/content/")
          (denote-prompts . (subdirectory file-type title keywords))
-         (denote-file-type . markdown-toml))))
+         (denote-file-type . markdown-toml)
+         (auto-insert-mode . t)
+         (denote-toml-front-matter . "+++\ntitle      = %s\ndate       = %s\ntags       = %s\nidentifier = %S\nslug       = \"\"\nsummary    = \"This week is not over, so this entry is incomplete.\"\n+++\n\n")
+         )))
